561ab334f95fd2dc74ae579267133b260b497b4f
zx4rax <zx4rax@proton.me>
2026-05-15 13:30:44 +0330

    update version

--- env/.config/nvim/lua/theprimeagen/lazy/treesitter.lua
+++ env/.config/nvim/lua/theprimeagen/lazy/treesitter.lua
@@ -7,7 +7,7 @@ return {
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash",
+                "jsdoc", "bash","go",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
